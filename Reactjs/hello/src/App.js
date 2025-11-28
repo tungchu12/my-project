b1b246af13ecb13bd835ca210cb7bcf99f1ec900
tungchu12 <chutung2004.com>
2025-11-28 09:04:10 +0700

    update

--- Reactjs/hello/src/App.js
+++ Reactjs/hello/src/App.js
@@ -1,18 +1,118 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Hello world !
-        </p>
-        
-        
+//---------JSX và Component trong React------------
+export function Headers(){
+  return(
+    <div>
+      <header className="header">
+        <h1>Đây là Header</h1>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export function Footers(){
+    return(
+    <div>
+      <footer className="footer">
+        <h1>Đây là Footer</h1>
+      </footer>
+    </div>
+  )
+}
+
+export function Showinfo(props){
+  return(
+    <section className="banner">
+      <div>
+        <h1>Thông tin Banner {props.name}</h1>
+      </div>
+    </section>
+  )
+}
+//---------Tạo một component Our Reputation nhận dữ liệu task qua props và hiển thị thông tin task.------------
+export const data =[
+  {
+    id:0,
+    icon: <img src="" alt="" />,
+    title: "Best Services",
+    content: "Nullam senectus porttitor in eget. Eget rutrum leo interdum.",
+  },
+  {
+    id:1,
+    icon: <img src="" alt="" />,
+    title: "Best Teams",
+    content: "Cursus semper tellus volutpat aliquet lacus. ",
+  },
+  {
+    id:2,
+    icon: <img src="" alt="" />,
+    title: "Best Designs",
+    content: "Ultricies at ipsum nunc, tristique nam lectus.",
+  }
+];
+
+export function OurReputation(props){
+  const items = props.data;
+  return(
+    <div className="reputation-wrapper">
+      {items.map((item) => (
+        <div key={item.id} className="reputation-item">
+          <div className="icon">{item.icon}</div>
+          <h3 className="title">{item.title}</h3>
+          <p className="content">{item.content}</p>
+        </div>
+      ))}
+    </div>
+  )
+}
+//----------Tạo một component Form để nhập thông tin-----
+export function Input({label , type}){
+  return(
+      <form action="">
+        <label >{label}</label>
+        <input type={type} />
+      </form>
+  )
+}
+
+export function Form(){
+  return(
+    <div className="form-info">
+      <Input
+        label ="Họ và tên"
+        type ="text"
+      />
+      <Input
+        label ="Số điện thoại"
+        type ="number"
+      />  
+      <Input
+        label= "Email"
+        type="email"
+      />
+      <Input
+        type="submit"
+      />
+    </div>
+  )
+}
+
+
+export function CountClick(){
+  const [count, setCount] = useState(0);
+  return(
+      <div className="countclick">
+        <button onClick={() =>setCount(count +1)}>Click</button>
+        <h1>Số lần Click:{count}</h1>
+      </div>
+  )
+}
+
+export function LoopClick({count}){
+  const items =0;
+  for(let i =0; i< count; i++) {
+       items.push(i);
+  }
+  return <div>{items}</div>;
+}
--- Reactjs/hello/src/index.js
+++ Reactjs/hello/src/index.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Headers, OurReputation } from './App';
+import { Footers } from './App';
+import { Showinfo } from './App';   
+import { data } from './App';  
+import { Form } from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import { CountClick } from './App';
+import { LoopClick } from './App';
+import { useState } from "react";
+const header = ReactDOM.createRoot(document.getElementById('header'));
+header.render(<Headers />);
+ 
+const footer = ReactDOM.createRoot(document.getElementById('footer'));
+footer.render(<Footers />);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const banner = ReactDOM.createRoot(document.getElementById('banner'));
+banner.render(<Showinfo  name ="123"/>);
+
+const reputation = ReactDOM.createRoot(document.getElementById('our_reputation'));
+reputation.render(<OurReputation data={data}/>);
+
+const form = ReactDOM.createRoot(document.getElementById('form'));
+form.render(<Form />);
+
+const countclick = ReactDOM.createRoot(document.getElementById('count_click'));
+countclick.render(<CountClick />)
